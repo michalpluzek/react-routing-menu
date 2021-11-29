a08efb994b8a745c508209840e9804c5a91af41b
Michał Płużek <michalpluzek@gmail.com>
2021-11-29 10:48:56 +0100

    Add components

---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+  return <h1>Kontakt do nas</h1>;
+};
+
+export default Contact;
---
+++ src/components/News.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const News = () => {
+  return <h1>Aktualności</h1>;
+};
+
+export default News;
