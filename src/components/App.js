779da8d423d9c04572ef3d6f8ef2b7d29e66fa05
Michał Płużek <michalpluzek@gmail.com>
2021-11-29 10:49:05 +0100

    Add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import News from "./News";
+import Contact from "./Contact";
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div>
           <header>
             <nav>
@@ -22,9 +25,15 @@ class App extends React.Component {
               </ul>
             </nav>
           </header>
-          <section>Strona - Witaj</section>
+          <section>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </section>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
