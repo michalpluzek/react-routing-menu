f63a78fe065b24bdc14c56ec5d2076c20fb9be4f
Michał Płużek <michalpluzek@gmail.com>
2021-11-29 10:31:49 +0100

    Add simply menu template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Link } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-    return <div>React Router</div>;
+    return (
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Start</Link>
+                </li>
+                <li>
+                  <Link to="/news">Aktualności</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Kontakt</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <section>Strona - Witaj</section>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
