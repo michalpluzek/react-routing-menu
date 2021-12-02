7a635ce91072230dfb05bb571ef189934abf28ac
Michał Płużek <michalpluzek@gmail.com>
2021-12-02 09:41:33 +0100

    Refactoring to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -11,48 +11,46 @@ import News from "./News";
 import Contact from "./Contact";
 import ErrorPage from "./ErrorPage";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <header>
-            <nav>
-              <ul>
-                <li>
-                  <NavLink to="/">Start</NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="/news"
-                    className={({ isActive }) => isActive && "news_selected"}
-                  >
-                    Aktualności
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="/contact"
-                    className={({ isActive }) => isActive && "contact_selected"}
-                  >
-                    Kontakt
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <section>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/news" element={<News />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="*" element={<ErrorPage />} />
-            </Routes>
-          </section>
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Start</NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/news"
+                  className={({ isActive }) => isActive && "news_selected"}
+                >
+                  Aktualności
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) => isActive && "contact_selected"}
+                >
+                  Kontakt
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </section>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
